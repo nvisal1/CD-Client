15670241f28567bbdd004762d1386471adce9c89
Paige <pz0342@gmail.com>
2019-03-15 11:55:42 -0400

    Add homepage component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './homepage/header/header.component';
+import { FooterComponent } from './homepage/footer/footer.component';
+import { BodyComponent } from './homepage/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
